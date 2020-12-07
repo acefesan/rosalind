9c8edd67b31e11d59e940dcaed3a9058b2c5aec4
Adrian Sanchez <adriancefsanchez@gmail.com>
2020-12-07 05:42:04 +0000

    finished GC OMGGG I'm so excited!

--- stronghold/GC/GC.py
+++ stronghold/GC/GC.py
@@ -1,6 +1,35 @@
+import operator
 
-dataset = open("stronghold/GC/rosalind_fib.txt","r")
 
-S = dataset.readlines()[0]
-S = S.strip().split(' ')
+
+dataset = open("stronghold/GC/rosalind_gc.txt","r")
+
+S = dataset.read()
+S = S.split('>')
+S = S[1:]
+
+D = {}
+
+
+
+for entry in S:
+  split = entry.split('\n')
+  index = split[0]
+  seq = ''.join(split[1:])
+  D[index] = seq
+
+
+def gc(s):
+  return (s.count('G') + s.count('C'))/len(s)*100
+
+for k in D:
+  D[k] = gc(D[k])
+
+
+carissawouldhavedonethisfaster= max(D.items(),key=operator.itemgetter(1))
+
+withbettervariablenames = carissawouldhavedonethisfaster[1]
+
+print(carissawouldhavedonethisfaster[0])
+print(withbettervariablenames)
 
